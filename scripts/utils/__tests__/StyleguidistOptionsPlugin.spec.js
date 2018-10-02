c85933c54ffb19b0903528075e4b9cc8ba6bb82c
Marcin Lichwała <marcin.lichwala@tomtom.com>
2018-10-02 07:43:49 +0200

    Fix: Drop webpack 3 support

--- scripts/utils/__tests__/StyleguidistOptionsPlugin.spec.js
+++ scripts/utils/__tests__/StyleguidistOptionsPlugin.spec.js
@@ -7,12 +7,16 @@ const options = {
 it('should attach Styleguidist config object to webpack context', () => {
 	const context = {};
 	const compiler = {
-		plugin: (step, callback) => {
-			callback({
-				plugin: (step, callback) => {
-					callback(context, { resource: 'pizza' });
+		hooks: {
+			compilation: {
+				tap(pluginName, callback) {
+					callback({
+						plugin: (step, callback) => {
+							callback(context, { resource: 'pizza' });
+						},
+					});
 				},
-			});
+			},
 		},
 	};
 	const plugin = new StyleguidistOptionsPlugin(options);
@@ -24,12 +28,16 @@ it('should attach Styleguidist config object to webpack context', () => {
 it('should do nothing when resource is empty', () => {
 	const context = {};
 	const compiler = {
-		plugin: (step, callback) => {
-			callback({
-				plugin: (step, callback) => {
-					callback(context, {});
+		hooks: {
+			compilation: {
+				tap(pluginName, callback) {
+					callback({
+						plugin: (step, callback) => {
+							callback(context, {});
+						},
+					});
 				},
-			});
+			},
 		},
 	};
 	const plugin = new StyleguidistOptionsPlugin(options);
